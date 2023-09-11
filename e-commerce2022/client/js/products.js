ae13d96f0b419a30a91b54e479314b0a1a913a87
Carlos Hernan <86528810+Hernan-DOS@users.noreply.github.com>
2023-09-11 00:05:33 -0300

    Add files via upload

---
+++ e-commerce2022/client/js/products.js
@@ -0,0 +1,30 @@
+const productos = [
+    {
+        id: 1,
+        productName: "suscripcion1",
+        price: 100,
+        quanty: 1,
+        img: "/client/media/suscripcion1.jpg",
+    },
+    {
+        id: 2,
+        productName: "suscripcion2",
+        price: 200,
+        quanty: 2,
+        img: "/client/media/suscripcion2.jpg",
+    },
+    {
+        id: 3,
+        productName: "suscripcion3",
+        price: 300,
+        quanty: 3,
+        img: "/client/media/suscripcion3.jpg",
+    },
+    {
+        id: 4,
+        productName: "suscripcion4",
+        price: 400,
+        quanty: 4,
+        img: "/client/media/suscripcion4.jpg",
+    },
+];
